c47777672f67b6a9804be55282f9cedf52031dca
degarashi <slice013@gmail.com>
2017-04-11 17:44:03 +0900

    Size: 比較関数

--- src/size.ts
+++ src/size.ts
@@ -1,4 +1,8 @@
 class Size {
 	constructor(public width:number, public height:number) {}
+	equal(s: Size): boolean {
+		return this.width === s.width &&
+				this.height === s.height;
+	}
 }
 export default Size;
