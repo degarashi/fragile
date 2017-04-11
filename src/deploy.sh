4f85b39fc79f9a526fdbd1d7a0a44b9f37bb7fa3
degarashi <slice013@gmail.com>
2017-04-11 19:40:30 +0900

    glslifyのエラーを消さない(/dev/nullに放り込まない)

--- src/deploy.sh
+++ src/deploy.sh
@@ -18,7 +18,7 @@ fi
 
 cd resource
 for file in `\find .  -maxdepth 1 -name "*.?sh" -type f -printf "%P "`; do
-	glslify $file -t glslify-import -o ../../dist/resource/$file 2>/dev/null
+	glslify $file -t glslify-import -o ../../dist/resource/$file
 	if [ $? -gt 0 ]; then
 		echo "glslify ERROR in ${file}"
 		exit 1
