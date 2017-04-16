aee47272b1bb4b7f4095b96a8cb6b5f4741ba6ad
degarashi <slice013@gmail.com>
2017-04-16 16:49:27 +0900

    Size: (w,h, (1/z), (1/w))の出力を行う関数

--- src/size.ts
+++ src/size.ts
@@ -1,8 +1,18 @@
+import Vec4 from "./vector4";
+
 class Size {
 	constructor(public width:number, public height:number) {}
 	equal(s: Size): boolean {
 		return this.width === s.width &&
 				this.height === s.height;
+	}
+	toVec4(): Vec4 {
+		return new Vec4(
+			this.width,
+			this.height,
+			1 / this.width,
+			1 / this.height
+		);
 	}
 }
 export default Size;
