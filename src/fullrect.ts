03ea396773a6bcedc9ebc9ebeffb1d325a261faf
degarashi <slice013@gmail.com>
2017-04-18 16:40:05 +0900

    FullRect: UV指定関連のバグ修正

--- src/fullrect.ts
+++ src/fullrect.ts
@@ -6,6 +6,8 @@ import Geometry from "./geometry";
 import {engine, gl} from "./global";
 import {DrawWithGeom} from "./utilfuncs";
 import GLTexture from "./gl_texture";
+import Rect from "./rect";
+import Vec2 from "./vector2";
 
 export default class FullRect extends DObject {
 	private _rect = <ResourceWrap<Geometry>>ResourceGen.get(new RPGeometry("Rect01_01"));
@@ -13,6 +15,7 @@ export default class FullRect extends DObject {
 	alpha: number = 1;
 	zoom: number = 1;
 	alphablend: boolean = false;
+	vflip: boolean = false;
 
 	onDraw(): void {
 		if(!this.texture)
@@ -20,7 +23,19 @@ export default class FullRect extends DObject {
 		engine.setTechnique(this.alphablend ? "rectAdd" : "rect");
 		engine.setUniform("u_texture", this.texture);
 		engine.setUniform("u_alpha", this.alpha);
-		engine.setUniform("u_zoom", 1/this.zoom);
+		const ts = this.texture.truesize();
+		const s = this.texture.size();
+		const uv = new Rect(0, s.height/ts.height, s.width/ts.width, 0);
+		engine.setUniform("u_uvcenter", uv.center());
+		const vf = this.vflip ? -1 : 1;
+		const zi = 1/this.zoom;
+		engine.setUniform(
+			"u_uvratio",
+			new Vec2(
+				uv.width()/2*zi,
+				uv.height()/2*vf*zi
+			)
+		);
 		engine.draw(()=> {
 			DrawWithGeom(this._rect.data, gl.TRIANGLES);
 		});
--- src/resource/rectf.fsh
+++ src/resource/rectf.fsh
@@ -3,12 +3,9 @@ precision mediump float;
 
 uniform sampler2D u_texture;
 uniform float u_alpha;
-uniform float u_zoom;
 varying vec2 v_uv;
 
 void main() {
-	vec2 uv = v_uv * u_zoom;
-	uv = uv*0.5+0.5;
-	vec4 c = texture2D(u_texture, uv);
+	vec4 c = texture2D(u_texture, v_uv);
 	gl_FragColor = vec4(c.rgb, u_alpha);
 }
