ae4feb005601fac5176ad075738c95c57af1741a
degarashi <slice013@gmail.com>
2017-04-25 17:03:48 +0900

    ImageLoader: データをXHRHttpRequestで読み込む

--- fragile/image_loader.ts
+++ fragile/image_loader.ts
@@ -1,53 +1,28 @@
-import ResourceLoader from "./resource_loader";
 import RLoaderCB from "./rloader_cb";
+import XHRLoader from "./xhr_loader";
 
-export default class ImageLoader implements ResourceLoader {
-	private _url: string;
-	private _timerId: number|null;
-	private _status: string;
-	private _cb: RLoaderCB;
+export default class ImageLoader extends XHRLoader {
 	private _img: HTMLImageElement;
-	private _errormsg: string;
 
 	constructor(url: string) {
-		const img = new Image();
-		img.onload = ()=> {
-			if(this._timerId) {
-				this._timerId = null;
-				this._status = "complete";
-				this._cb.progress(1, 1);
-				this._cb.completed();
-			}
-		};
-		this._url = url;
-		this._img = img;
-		this._status = "idle";
+		super(url, "blob");
 	}
 	begin(callback: RLoaderCB, timeout: number) {
-		if(timeout < 0)
-			timeout = 60000;
-		this._timerId = setTimeout(()=> {
-			if(this._timerId) {
-				// timeout
-				this._status = "error";
-				this._errormsg = "connection timedout";
-				this._cb.error();
-			}
-		}, timeout);
-		this._cb = callback;
-		this._img.src = this._url;
-		this._status = "loading";
-		callback.progress(0, 1);
-	}
-	abort() {
-		this._timerId = null;
-		this._status = "abort";
-	}
-	errormsg(): string {
-		return this._errormsg;
-	}
-	status(): string {
-		return this._status;
+		super.begin(
+			{
+				completed: ()=> {
+					const img = new Image();
+					this._img = img;
+					img.src = window.URL.createObjectURL(super.result());
+					img.onload = ()=> {
+						callback.completed();
+					};
+				},
+				error: callback.error,
+				progress: callback.progress
+			},
+			timeout
+		);
 	}
 	result(): any {
 		return this._img;
